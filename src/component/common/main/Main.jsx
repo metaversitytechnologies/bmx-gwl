001108eda0a6874f8f58424c183ae08b7fa362e6
ritu-metaversity <rituraj@metaversitytechnologies.com>
2023-10-10 17:54:09 +0530

    Is Self App url Change

--- src/component/common/main/Main.jsx
+++ src/component/common/main/Main.jsx
@@ -57,9 +57,11 @@ import NewCreateUser from "../../pages/newCreateUser/NewCreateUser";
 
 const Main = ({setOpenRules}) => {
 
-  const {data: logoData} = useIt_Self_By_APP_URLQuery({
-    appUrl: window.location.hostname, 
-  // appUrl: "subadmin.11bet24.com",
+  let appUrl = (window.location.hostname).split(".");
+  appUrl.shift();
+  appUrl=appUrl.join(".");
+  const {data: logoData} = useIt_Self_By_APP_URLQuery({ 
+    appUrl
 }, {refetchOnMountOrArgChange: true});
 
 useEffect(() => {
