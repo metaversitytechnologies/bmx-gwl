5f9a586379b5af2ecd474a430ee02ede4fb781da
ritu-metaversity <rituraj@metaversitytechnologies.com>
2023-12-04 16:25:34 +0530

    new changes on create user page

--- src/component/pages/newCreateUser/CasinoCommission.jsx
+++ src/component/pages/newCreateUser/CasinoCommission.jsx
@@ -57,22 +57,22 @@ const CasinoCommission = ({createName, commiType}) => {
                               value: "1.00",
                               label: "1.00",
                             },
-                            {
-                              value: "1.25",
-                              label: "1.25",
-                            },
-                            {
-                              value: "1.50",
-                              label: "1.50",
-                            },
-                            {
-                              value: "1.75",
-                              label: "1.75",
-                            },
-                            {
-                              value: "2.00",
-                              label: "2.00",
-                            },
+                            // {
+                            //   value: "1.25",
+                            //   label: "1.25",
+                            // },
+                            // {
+                            //   value: "1.50",
+                            //   label: "1.50",
+                            // },
+                            // {
+                            //   value: "1.75",
+                            //   label: "1.75",
+                            // },
+                            // {
+                            //   value: "2.00",
+                            //   label: "2.00",
+                            // },
                           ]}
                         />
                       </Form.Item>
